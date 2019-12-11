1d629be49df34be6c54d3f2ce66bbacd51ddf3f5
Ованес Гаранян <o.garanyan@itgrp.ru>
2019-12-11 14:58:43 +0300

    fix multiple edges

--- Algorithms/task_12.cpp
+++ Algorithms/task_12.cpp
@@ -16,12 +16,14 @@ public:
     bool BridgesCount(std::string output_file);
 
 private:
-    void
-    dfs(size_t i, std::vector<bool> &visited, std::vector<int> &disc, std::vector<int> &low,
-        std::vector<std::pair<size_t, size_t>> &bridges, int p = -1);
+    void dfs(size_t i, std::vector<bool>& visited, std::vector<int>& disc, std::vector<int>& low,
+             std::vector<std::pair<size_t, size_t>>& bridges, int p = -1);
 
+    //Вершина на список смежных вершин
     std::vector<std::vector<size_t>> vertex_to_neighbour;
-    std::map<std::pair<size_t, size_t>, size_t> edges;
+
+    //ребро на список его индексов во входном файле
+    std::map<std::pair<size_t, size_t>, std::vector<size_t>> edges;
     size_t vertices_num = 0;
 };
 
@@ -42,14 +44,15 @@ void Graph::AddEdge(size_t from, size_t to, size_t id)
 
     vertex_to_neighbour[from].push_back(to);
     vertex_to_neighbour[to].push_back(from);
-    edges[std::make_pair(from,to)] = id;
+    auto edge = std::make_pair(from, to);
+    edges[edge].emplace_back(id);
 }
 
 bool Graph::BridgesCount(std::string output_file)
 {
 
     std::vector<bool> visited(vertices_num, false);
-    std::vector<int> disc(vertices_num,-1);
+    std::vector<int> disc(vertices_num, -1);
     std::vector<int> low(vertices_num, -1);
 
     std::vector<std::pair<size_t, size_t>> bridges;
@@ -59,38 +62,31 @@ bool Graph::BridgesCount(std::string output_file)
 
     std::ofstream output;
     output.open(output_file, std::ios::out);
-    output << bridges.size() << std::endl;
+
     std::vector<size_t> result;
     result.reserve(bridges.size());
-    for (const auto &bridge: bridges)
+    for (const auto& bridge: bridges)
     {
-//        auto it = std::find_if(edges.begin(), edges.end(),
-//                               [&bridge](const std::pair<size_t, size_t> &element)
-//                               { return element.first == bridge.first && element.second == bridge.second; });
-//        if (it == edges.end())
-//        {
-//            it = std::find_if(edges.begin(), edges.end(),
-//                              [&bridge](const std::pair<size_t, size_t> &element)
-//                              { return element.first == bridge.second && element.second == bridge.first; });
-//        }
         auto it = edges.find(bridge);
-        if(it != edges.end())
-            result.emplace_back(edges[bridge]);
-        else
-            result.emplace_back(edges[std::make_pair(bridge.second,bridge.first)]);
+        if (it != edges.end() && it->second.size() == 1)
+            result.insert(result.end(), edges[bridge].begin(), edges[bridge].end());
+        else if (edges[std::make_pair(bridge.second, bridge.first)].size() == 1)
+            result.insert(result.end(), edges[std::make_pair(bridge.second, bridge.first)].begin(),
+                edges[std::make_pair(bridge.second, bridge.first)].end());
     }
+    output << result.size() << std::endl;
     std::sort(result.begin(), result.end());
-    for(const auto r : result)
+    for (const auto r : result)
     {
-        output<<r<<std::endl;
+        output << r << std::endl;
     }
     output.close();
     return true;
 
 }
 
-void Graph::dfs(size_t id, std::vector<bool> &visited, std::vector<int> &disc,
-                std::vector<int> &low, std::vector<std::pair<size_t, size_t>> &bridges, int p)
+void Graph::dfs(size_t id, std::vector<bool>& visited, std::vector<int>& disc,
+                std::vector<int>& low, std::vector<std::pair<size_t, size_t>>& bridges, int p)
 {
     static int time = 0;
 
@@ -99,18 +95,18 @@ void Graph::dfs(size_t id, std::vector<bool> &visited, std::vector<int> &disc,
 
     for (const auto neighbour: vertex_to_neighbour[id])
     {
-//        if(neighbour == p)
-//            continue;
-        if(!visited[neighbour])
+        if (!visited[neighbour])
         {
             dfs(neighbour, visited, disc, low, bridges, id);
-            if(low[id] > low[neighbour] )
-                low[id]= low[neighbour];
-            if(low[neighbour]==disc[neighbour])
+            if (low[id] > low[neighbour])
+                low[id] = low[neighbour];
+            if (low[neighbour] == disc[neighbour])
                 bridges.emplace_back(id, neighbour);
 
-        } else if (neighbour != p){
-            if(low[id] > disc[neighbour])
+        }
+        else if (neighbour != p)
+        {
+            if (low[id] > disc[neighbour])
                 low[id] = disc[neighbour];
 
         }
@@ -130,10 +126,10 @@ int main()
         {
             size_t from, to;
             file >> from >> to;
-            graph.AddEdge(from - 1, to - 1, i+1);
+            graph.AddEdge(from - 1, to - 1, i + 1);
         }
     }
     file.close();
-    graph.BridgesCount("output.txt");
+    graph.BridgesCount("bridges.out");
     return 0;
 }
